59e8d88835d75943356061f146158c08f6da20f2
jhotiori <martiori.jho@gmail.com>
2026-04-10 18:43:58 -0300

    misc(Categorias): melhorias no repository e service

--- src/main/java/org/javapi/sigob/service/CategoriaService.java
+++ src/main/java/org/javapi/sigob/service/CategoriaService.java
@@ -1,47 +1,45 @@
 package org.javapi.sigob.service;
 
+import java.util.List;
+
 import org.javapi.sigob.entity.Categoria;
 import org.javapi.sigob.exception.CategoriaException;
 import org.javapi.sigob.repository.CategoriaRepository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityTransaction;
-import java.util.List;
-
 public class CategoriaService {
     private final CategoriaRepository repository;
 
+    /**
+     * Construtor para criar um novo CategoriaService
+     *
+     * @param repository O repositorio do mesmo
+     * @return CategoriaService - O novo CategoriaService
+     */
     public CategoriaService(CategoriaRepository repository) {
         this.repository = repository;
     }
 
-    public void save(Categoria categoria, EntityManager em) {
+    /**
+     * Salva uma nova Categoria
+     *
+     * @param categoria A Categoria para ser salva
+     */
+    public void save(Categoria categoria) {
         validateCategoria(categoria);
 
-        EntityTransaction tx = em.getTransaction();
-        try {
-            tx.begin();
-            int idCategoria = categoria.getIdCategoria();
-
-            if (idCategoria > 0) {
-                this.repository.update(categoria);
-            } else {
-                this.repository.create(categoria);
-            }
-            tx.commit();
-        } catch (Exception e) {
-            if (tx.isActive()) {
-                tx.rollback();
-            }
-            throw e;
+        if (categoria.getIdCategoria() > 0) {
+            this.repository.update(categoria);
+        } else {
+            this.repository.save(categoria);
         }
     }
 
-    public void create(Categoria categoria) {
-        validateCategoria(categoria);
-        this.repository.create(categoria);
-    }
-
+    /**
+     * Atualiza uma Categoria
+     *
+     * @param categoria A Categoria para ser atualizada
+     * @throws CategoriaException Se o ID da Categoria for menor ou igual a zero
+     */
     public void update(Categoria categoria) {
         validateCategoria(categoria);
         if (categoria.getIdCategoria() <= 0) {
@@ -50,29 +48,68 @@ public class CategoriaService {
         this.repository.update(categoria);
     }
 
-    public Categoria getById(int id) {
+    /**
+     * Remove uma Categoria
+     *
+     * @param categoria A Categoria para ser removida
+     */
+    public void delete(Categoria categoria) {
+        this.repository.delete(categoria);
+    }
+
+    /**
+     * Confere se uma categoria existe
+     *
+     * @param categoria A categoria
+     * @return boolean - true se a categoria existe, false se nao
+     */
+    public boolean contains(Categoria categoria) {
+        return this.repository.contains(categoria);
+    }
+
+    /**
+     * Retorna uma lista com todas as Categorias
+     *
+     * @return List<Categoria> - A lista de categorias
+     */
+    public List<Categoria> findAll() {
+        return this.repository.findAll();
+    }
+
+    /**
+     * Busca uma Categoria pelo seu ID
+     *
+     * @param id O ID da Categoria
+     * @return Categoria - A Categoria buscada
+     * @throws CategoriaException Se o ID da Categoria for menor ou igual a zero
+     */
+    public Categoria findById(int id) {
         if (id <= 0) {
             throw new CategoriaException("ID da categoria não pode ser menor ou igual a zero");
         }
         return this.repository.findById(id);
     }
 
-    public List<Categoria> getByNome(String nome) {
+    /**
+     * Busca por categorias que comecam com o prefixo (nome)
+     *
+     * @param nome O prefixo
+     * @return List<Categoria> - A lista de categorias
+     */
+    public List<Categoria> findByNome(String nome) {
         validateNome(nome);
         return this.repository.findByName(nome);
     }
 
-    public Categoria getByCodigo(String codigo) {
+    /**
+     * Busca por uma Categoria pelo seu codigo
+     *
+     * @param codigo O codigo da categoria
+     * @return Categoria - A categoria buscada
+     */
+    public Categoria findByCodigo(String codigo) {
         validateCodigo(codigo);
         return this.repository.findByCodigo(codigo);
-    }
-
-    public List<Categoria> getAll() {
-        return this.repository.findAll();
-    }
-
-    public void delete(Categoria categoria) {
-        this.repository.delete(categoria);
     }
 
     private void validateCategoria(Categoria categoria) {
