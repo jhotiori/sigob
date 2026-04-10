d41956b843e1aeec4e868fc16474368070023aae
jhotiori <martiori.jho@gmail.com>
2026-04-10 19:42:24 -0300

    misc(Estoque): melhorias no service e repo

--- src/main/java/org/javapi/sigob/repository/EstoqueRepository.java
+++ src/main/java/org/javapi/sigob/repository/EstoqueRepository.java
@@ -5,41 +5,114 @@ import java.util.List;
 import org.javapi.sigob.entity.Estoque;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 
 public class EstoqueRepository {
-    private EntityManager em;
+    private final EntityManager em;
 
+    /**
+     * Cria um novo EstoqueRepository
+     * @param em O EntityManager
+     * @return EstoqueRepository - Um novo EstoqueRepository
+     */
     public EstoqueRepository(EntityManager em) {
         this.em = em;
     }
 
-    public void create(Estoque estoque) {
-        em.persist(estoque);
+    /**
+     * Salva um estoque
+     * @param estoque O estoque para ser salvo
+     */
+    public void save(Estoque estoque) {
+        EntityManager manager = this.em;
+        EntityTransaction transaction = manager.getTransaction();
+
+        try {
+            transaction.begin();
+            manager.persist(estoque);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw e;
+        }
     }
 
+    /**
+     * Atualiza um estoque
+     * @param estoque O estoque para ser atualizado
+     */
     public void update(Estoque estoque) {
-        em.merge(estoque);
+        EntityManager manager = this.em;
+        EntityTransaction transaction = manager.getTransaction();
+
+        try {
+            transaction.begin();
+            manager.merge(estoque);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw e;
+        }
     }
 
+    /**
+     * Confere se um estoque existe
+     * @param estoque O estoque para conferir
+     * @return boolean - true se o estoque existe, false se nao
+     */
     public boolean contains(Estoque estoque) {
         return em.contains(estoque);
     }
 
+    /**
+     * Deleta um estoque
+     * @param estoque O estoque para ser deletado
+     */
     public void delete(Estoque estoque) {
-        em.remove(em.contains(estoque) ? estoque : em.merge(estoque));
+        EntityManager manager = this.em;
+        EntityTransaction transaction = manager.getTransaction();
+
+        try {
+            transaction.begin();
+            manager.remove(manager.contains(estoque) ? estoque : manager.merge(estoque));
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw e;
+        }
     }
 
+    /**
+     * Retorna uma lista com todos os estoques
+     * @return List<Estoque> - A lista de estoques
+     */
     public List<Estoque> findAll() {
         return em.createQuery("select e from estoques e", Estoque.class).getResultList();
     }
 
-    public List<Estoque> findByName(String prefixo) {
-        return em.createQuery("select e from estoques e where e.nmEstoque like :prefix", Estoque.class)
-                .setParameter("prefix", prefixo + "%")
-                .getResultList();
-    }
-
+    /**
+     * Busca um estoque pelo id
+     * @param id O id do estoque
+     * @return Estoque - O estoque encontrado
+     */
     public Estoque findById(int id) {
         return em.find(Estoque.class, id);
+    }
+
+    /**
+     * Busca estoques pelo prefixo
+     * @param nome O prefixo do estoque
+     * @return List<Estoque> - A lista de estoques encontrados
+     */
+    public List<Estoque> findByNome(String nome) {
+        return em.createQuery("select e from estoques e where e.nmEstoque like :prefix", Estoque.class)
+                .setParameter("prefix", nome + "%")
+                .getResultList();
     }
 }
--- src/main/java/org/javapi/sigob/service/EstoqueService.java
+++ src/main/java/org/javapi/sigob/service/EstoqueService.java
@@ -2,83 +2,100 @@ package org.javapi.sigob.service;
 
 import java.util.List;
 
-import org.javapi.sigob.config.JPAConfig;
 import org.javapi.sigob.entity.Estoque;
 import org.javapi.sigob.exception.EstoqueException;
 import org.javapi.sigob.repository.EstoqueRepository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityTransaction;
-
 public class EstoqueService {
     private final EstoqueRepository repository;
 
+    /**
+     * Cria um novo EstoqueService
+     * @param repository O repositorio de estoques
+     * @return EstoqueService - O serviço de estoques
+     */
     public EstoqueService(EstoqueRepository repository) {
         this.repository = repository;
     }
 
+    /**
+     * Salva um estoque
+     * @param estoque O estoque para ser salvo
+     * @throws EstoqueException Se o estoque for invalido
+     */
     public void save(Estoque estoque) {
         validateEstoque(estoque);
-        EntityManager em = JPAConfig.getEntityManager();
-        EntityTransaction tx = em.getTransaction();
-
-        try {
-            tx.begin();
-            this.repository.create(estoque);
-            tx.commit();
-        } catch (Exception e) {
-            tx.rollback();
-            throw e;
-        } finally {
-            em.close();
-        }
+        this.repository.save(estoque);
     }
 
+    /**
+     * Atualiza um estoque
+     * @param estoque O estoque para ser atualizado
+     * @throws EstoqueException Se o estoque for invalido
+     */
     public void update(Estoque estoque) {
         validateEstoque(estoque);
-        EntityManager em = JPAConfig.getEntityManager();
-        EntityTransaction tx = em.getTransaction();
-
-        try {
-            tx.begin();
-            this.repository.update(estoque);
-            tx.commit();
-        } catch (Exception e) {
-            tx.rollback();
-            throw e;
-        } finally {
-            em.close();
-        }
+        this.repository.update(estoque);
     }
 
+    /**
+     * Deleta um estoque
+     * @param estoque O estoque para ser deletado
+     */
     public void delete(Estoque estoque) {
         if (this.repository.contains(estoque)) {
             this.repository.delete(estoque);
         }
     }
 
+    /**
+     * Confere se um estoque existe
+     * @param estoque O estoque para conferir
+     * @return boolean - true se o estoque existe, false se nao
+     */
     public boolean contains(Estoque estoque) {
         return this.repository.contains(estoque);
     }
 
+    /**
+     * Retorna uma lista com todos os estoques
+     * @return List<Estoque> - A lista de estoques
+     */
     public List<Estoque> findAll() {
         return this.repository.findAll();
     }
 
-    public List<Estoque> findByName(String prefixo) {
-        return this.repository.findByName(prefixo);
-    }
-
+    /**
+     * Busca um estoque pelo id
+     * @param id O id do estoque
+     * @return Estoque - O estoque encontrado
+     */
     public Estoque findById(int id) {
         return this.repository.findById(id);
+    }
+
+    /**
+     * Busca estoques pelo prefixo
+     * @param prefixo O prefixo do estoque
+     * @return List<Estoque> - A lista de estoques encontrados
+     */
+    public List<Estoque> findByNome(String prefixo) {
+        return this.repository.findByNome(prefixo);
     }
 
     private void validateEstoque(Estoque estoque) {
         if (estoque == null) {
             throw new EstoqueException("Estoque não pode ser nulo");
         }
+        validateId(estoque.getIdEstoque());
         validateCodigo(estoque.getCdEstoque());
         validateNome(estoque.getNmEstoque());
+    }
+
+    private void validateId(int id) {
+        if (id <= 0) {
+            throw new EstoqueException("Id do estoque não pode ser menor ou igual a zero");
+        }
     }
 
     private void validateCodigo(String codigo) {
