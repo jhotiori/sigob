a2937df2b9f14e11c979d5c5b051b5e410f3c652
jhotiori <martiori.jho@gmail.com>
2026-04-10 22:19:02 -0300

    update denovo

--- src/main/java/org/javapi/sigob/Main.java
+++ src/main/java/org/javapi/sigob/Main.java
@@ -1,9 +1,11 @@
 package org.javapi.sigob;
 
 import org.javapi.sigob.cli.MenuMain;
+import org.javapi.sigob.config.FlywayConfig;
 
 public class Main {
     public static void main(String[] args) {
+        FlywayConfig.migrate();
         new MenuMain().exibir();
     }
 }
---
+++ src/main/resources/db/migration/v1__init.sql
@@ -0,0 +1,89 @@
+create table categorias (
+	idCategoria serial not null primary key,
+	cdCategoria varchar(50) not null unique,
+	nmCategoria varchar(100) not null
+);
+
+create table moedas (
+	idMoeda serial not null primary key,
+	nmMoeda varchar(100) not null,
+	dsCifrao varchar(50) not null,
+	dsSigla varchar(50) not null
+);
+
+create table produtos (
+	idProduto serial not null primary key,
+	cdProduto varchar(50) not null unique,
+	nmProduto varchar(100) not null,
+	dsProduto varchar(200),
+	vlCusto decimal(15,2),
+	vlProduto decimal(15,2),
+	fk_idCategoria int not null,
+	foreign key (fk_idCategoria) references categorias(idCategoria),
+	fk_idMoeda int not null,
+	foreign key (fk_idMoeda) references moedas(idMoeda)
+);
+
+
+create table estoques (
+	idEstoque serial not null primary key,
+	cdEstoque varchar(50) not null unique,
+	nmEstoque varchar(100) not null,
+	dsEstoque varchar(200),
+	fk_idCategoria int not null,
+	foreign key (fk_idCategoria) references categorias(idCategoria)
+
+);
+
+create table produtosEstoques (
+	idProdutoEstoque serial not null primary key,
+	nrQuantidade bigint not null,
+	dsObservacao varchar(200),
+	fk_idProduto int not null,
+	foreign key (fk_idProduto) references produtos(idProduto),
+	fk_idEstoque int not null,
+	foreign key (fk_idEstoque) references estoques(idEstoque)
+
+);
+
+create table clientes (
+	idCliente serial not null primary key,
+	nmCliente varchar(100) not null,
+	nrDocumento varchar(50)
+);
+
+create table acessos (
+	idAcesso serial not null primary key,
+	nmAcesso varchar(100) not null,
+	cdAcesso varchar(50) not null unique,
+	dsAcesso varchar(200)
+);
+
+create table funcionarios (
+	idFuncionario serial not null primary key,
+	nmFuncionario varchar(100) not null,
+	cdFuncionario varchar(50) not null unique,
+	fk_idAcesso int not null,
+	foreign key (fk_idAcesso) references acessos(idAcesso)
+);
+
+create table vendas (
+	idVenda serial not null primary key,
+	dtVenda timestamptz not null,
+	vlVenda decimal(15,2) not null,
+	flPago bool not null,
+	fk_idCliente int not null,
+	foreign key (fk_idCliente) references clientes(idCliente),
+	fk_idFuncionario int not null,
+	foreign key (fk_idFuncionario) references funcionarios(idFuncionario)
+);
+
+create table produtosVendas (
+	idProdutoVenda serial not null primary key,
+	nrQuantidade bigint not null check(nrQuantidade > 0),
+	vlSaldo decimal(15,2) not null,
+	fk_idProdutoEstoque int not null,
+	foreign key (fk_idProdutoEstoque) references produtosEstoques(idProdutoEstoque),
+	fk_idVenda int not null,
+	foreign key (fk_idVenda) references vendas(idVenda)
+);
