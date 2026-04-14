bfa3e30538e349706e77126fa85a92f63ad626e0
jhotiori <martiori.jho@gmail.com>
2026-04-14 10:55:46 -0300

    add(MenuConfig): configuração para menus

---
+++ src/main/java/org/javapi/sigob/config/MenuConfig.java
@@ -0,0 +1,7 @@
+package org.javapi.sigob.config;
+
+public class MenuConfig {
+    private MenuConfig() {}
+    public static final String LINE_SYMBOL = "─";
+    public static final String LINE = LINE_SYMBOL.repeat(25);
+}
