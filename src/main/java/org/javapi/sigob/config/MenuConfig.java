faa8a1a6eb8ad6daa1769ba26d587f2f6a895046
jhotiori <martiori.jho@gmail.com>
2026-04-14 14:09:33 -0300

    refactor(MenuConfig): ajuste na configuracao e organizacao dos menus

--- src/main/java/org/javapi/sigob/config/MenuConfig.java
+++ src/main/java/org/javapi/sigob/config/MenuConfig.java
@@ -1,7 +1,28 @@
 package org.javapi.sigob.config;
 
-public class MenuConfig {
+public final class MenuConfig {
     private MenuConfig() {}
+
     public static final String LINE_SYMBOL = "─";
-    public static final String LINE = LINE_SYMBOL.repeat(25);
+    public static final int LINE_SIZE = 25;
+
+    public static final String LABEL_EXIT = "Sair";
+    public static final String LABEL_PROMPT = "Insira uma opção: ";
+
+    public static String line() {
+        return LINE_SYMBOL.repeat(LINE_SIZE);
+    }
+
+    public static String banner(String titulo) {
+        String line = line();
+        return "%s %s %s".formatted(line, titulo, line);
+    }
+
+    public static String footer(String banner) {
+        return LINE_SYMBOL.repeat(banner.length());
+    }
+
+    public static String exitOption() {
+        return "[0] - %s".formatted(LABEL_EXIT);
+    }
 }
