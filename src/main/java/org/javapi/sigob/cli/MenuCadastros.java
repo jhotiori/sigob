807f22c815bf5ac823513c42dd6880b366329a8f
jhotiori <martiori.jho@gmail.com>
2026-04-14 14:09:33 -0300

    refactor(MenuCadastros): melhoria na estrutura e organizacao das entradas

--- src/main/java/org/javapi/sigob/cli/MenuCadastros.java
+++ src/main/java/org/javapi/sigob/cli/MenuCadastros.java
@@ -1,15 +1,14 @@
 package org.javapi.sigob.cli;
 
 public class MenuCadastros extends Menu {
-
     public MenuCadastros() {
         super("Operações de Cadastro");
-        adicionarEntrada("Acessos", () -> new MenuAcesso().exibir());
-        adicionarEntrada("Categorias", () -> new MenuCategoria().exibir());
-        adicionarEntrada("Clientes", () -> new MenuCliente().exibir());
-        adicionarEntrada("Funcionários", () -> new MenuFuncionario().exibir());
-        adicionarEntrada("Moedas", () -> new MenuMoeda().exibir());
-        adicionarEntrada("Produtos", () -> new MenuProduto().exibir());
-        adicionarEntrada("Estoques", () -> new MenuEstoque().exibir());
+        adicionarEntrada("Cadastrar Acessos", () -> new MenuAcesso().exibir());
+        adicionarEntrada("Cadastrar Categorias", () -> new MenuCategoria().exibir());
+        adicionarEntrada("Cadastrar Clientes", () -> new MenuCliente().exibir());
+        adicionarEntrada("Cadastrar Funcionários", () -> new MenuFuncionario().exibir());
+        adicionarEntrada("Cadastrar Moedas", () -> new MenuMoeda().exibir());
+        adicionarEntrada("Cadastrar Produtos", () -> new MenuProduto().exibir());
+        adicionarEntrada("Cadastrar Estoques", () -> new MenuEstoque().exibir());
     }
 }
