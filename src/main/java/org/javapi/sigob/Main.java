29e17ee1f0d3c460993c457104fedf1c4932153a
jhotiori <martiori.jho@gmail.com>
2026-04-27 00:38:51 -0300

    add(main): esqueci

--- src/main/java/org/javapi/sigob/Main.java
+++ src/main/java/org/javapi/sigob/Main.java
@@ -2,10 +2,11 @@ package org.javapi.sigob;
 
 import org.javapi.sigob.cli.MenuMain;
 import org.javapi.sigob.config.FlywayConfig;
+import org.javapi.sigob.service.FuncionarioService;
 
 public class Main {
     public static void main(String[] args) {
         FlywayConfig.migrate();
-        new MenuMain().exibir();
+        new MenuMain(new FuncionarioService()).show();
     }
 }
